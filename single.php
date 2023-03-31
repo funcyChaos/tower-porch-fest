859ee46aa98a5c890c91b5928a1855e1348e6331
neptali04 <monteznepti@gmail.com>
2023-03-30 22:15:33 -0700

    porches page single page

--- single.php
+++ single.php
@@ -9,32 +9,35 @@
 
 get_header();
 ?>
+	<section class="prochesSingle">
+		<main id="primary" class="site-main">
 
-	<main id="primary" class="site-main">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'towerpf-site' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'towerpf-site' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'towerpf-site' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'towerpf-site' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; // End of the loop.
+			?>
 
-		endwhile; // End of the loop.
-		?>
+			<a href="#" class="singleButton">SEE LINEUP</a>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
+	</section>
 <?php
 get_sidebar();
 get_footer();
