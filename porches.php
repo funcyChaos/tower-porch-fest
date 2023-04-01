368be374e536a6961f80a97fd356ac73d2a6020f
MyeishaT <trimblem852@gmail.com>
2023-03-31 17:00:16 -0700

    Finishing up styling for cards and adding categories

--- porches.php
+++ porches.php
@@ -8,7 +8,10 @@
 ?>
 
 <?php get_header(); ?>
-
+<div class="background">
+<h2 class="porch">Porches</h2>
+<p class="paragraph">Plan your day by exploring porch lineups. Listen to the bands ahead of time write down their porches. The organizers and performers are not paid, so we encourage all attendees to have cash to tip performers or purchase swag to help support the event.</p>
+</div>
 <section class="porchespost">
 
 <?php 
@@ -21,6 +24,8 @@
     
     while($args->have_posts()){
         $args->the_post();?>
+        
+        <div class="background">
          <div class="porchcard" id="<?php echo "card_". get_the_id();?>">
             <h2 class="porchheading" > <?php the_field('porch_name'); ?> </h2>
             <p class="porchparagraph"> <?php the_field('porch_address'); ?> </p>
@@ -28,12 +33,15 @@
 
             <div class="porchlinks"> 
 
-            <p><?php the_field('typegenre'); ?></p>
+            <p>categories here</p>
+            <p class="textoncard"><?php the_field('text_for_card'); ?></p>
+            <p class="timeoncard"><?php the_field('time_1'); ?> - <?php the_field('time_2'); ?></p>
             <a href="#" class="button7">SEE LINEUP</a>
 
             </div>
             
             </div>
+        </div>
     <?php }
 ?>
 </section>
