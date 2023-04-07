1340d4dd6441ec83b99e292fd33a6405c84b883f
Crystal Maldonado <cmaldonado@bitwiseindustries.com>
2023-04-07 12:16:56 -0700

    made the triangular part of polygon shape responsive

--- front-page-v2.php
+++ front-page-v2.php
@@ -8,15 +8,22 @@
 ?>
 
 <?php get_header(); ?>
+<main class="front-page-styles">
+  <?php
+  if ( has_post_thumbnail() ) {
+    /* grabs featured image */
+    $image_url = get_the_post_thumbnail_url();
+    echo '<div class="featured-image" style="background-image: url(' . $image_url . ');">';
+    echo '</div>';
+  }
+  ?>
+  <div class="polygon-wrapper">
+    <div class="rectangle"></div>
+    <div class="triangle"></div>
+  </div>
 
-<?php
-if ( has_post_thumbnail() ) {
-  /* grabs featured image */
-  $image_url = get_the_post_thumbnail_url();
-  echo '<div class="featured-image" style="background-image: url(' . $image_url . ');">';
-  echo '</div>';
-}
-?>
-<h1>ANTONIOOOO</h1>
+  <!-- <div class="house-shape"></div> -->
+
+</main>
 <!-- Footer must be included for navigation to display -->
 <?php get_footer(); ?>
