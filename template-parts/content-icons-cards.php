0fbefdace95a7d77afd7a25787dd933b5ffbf1fe
Crystal Maldonado <cmaldonado@bitwiseindustries.com>
2023-04-08 08:24:44 -0700

    cleaned up code

--- template-parts/content-icons-cards.php
+++ template-parts/content-icons-cards.php
@@ -8,16 +8,13 @@
  */
 
 ?>
-
-<!-- <div class="row"> -->
     <div class="col-sm-6 col-lg-3 mb-4">
         <div class="square">
           <img class="icon" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/26.Ticket.png" alt="">
         </div>
         <div class="content-wrapper container-fluid">
           <h2>Building Community Through Music and Art</h2>
-          <p>Porches throughout Fresno’s historic Tower neighborhood host performances in a day of revelry celebrating local music and art! We do this through the collaboration of Tower residents who provide venue for local musicians to perform.</p>
+          <p>Porches throughout Fresno's historic Tower neighborhood host performances in a day of revelry celebrating local music and art! We do this through the collaboration of Tower residents who provide venue for local musicians to perform.</p>
           <p class="text-link">View Porch Map</p>
         </div>
     </div>
-  <!-- </div> -->
