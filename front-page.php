6d5aed763d87d737b10cccfb57bde4f43d741da3
Crystal Maldonado <cmaldonado@bitwiseindustries.com>
2023-04-09 08:16:46 -0700

    Images and styles were broken for front page. Fix: Changed front-page.php to front-page-original.php. Updated front-page-v2.php to front-page.php. ''

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Homepage
+ * Template Name: Front Page v2
  *
  * @package towerpf-site
  *
@@ -8,260 +8,139 @@
 ?>
 
 <?php get_header(); ?>
-<div class="Home-page">
-
-    <?php
-
-        while ( have_posts() ) :
-            the_post();
-           
-            get_template_part( 'template-parts/content', 'page' );
-           
-                       
-        endwhile; // End of the loop.
-    ?>
-   
-</div>
-
-
-<div class="contain">
-        <img class="top" src="<?php the_field('top'); ?>" alt="">
-        <div class="center1"><?php the_field('location'); ?></div>
-        <div class="center2"><?php the_field('date'); ?></div>
-        <div class="center3"><?php the_field('title'); ?></div>
-        <div class="center4"><?php the_field('countdown'); ?></div>
-</div>
-
-<img class="middle" src="<?php the_field('middle'); ?>" alt="">
-
-<img class="bottom" src="<?php the_field('bottom'); ?>" alt="">
-
-<!-- Start of first featurette -->
-<div class="row featurette">
-      <div class="col-md-75">
-        <h2 class="featurette-heading"><?php the_field('heading_1'); ?><br> <?php the_field('heading_2'); ?></h2>
-        <p class="lead"><?php the_field('paragraph'); ?></p>
-        <a href="#" class="button">VIEW PORCH MAP</a>
+<main class="front-page-styles">
+  <?php
+  if ( has_post_thumbnail() ) {
+    /* grabs featured image */
+    $image_url = get_the_post_thumbnail_url();
+    echo '<div class="featured-image" style="background-image: url(' . $image_url . ');">';
+    echo '</div>';
+  }
+  ?>
+  <!-- date and counter -->
+  <div class="polygon-wrapper">
+    <div class="rectangle">
+      <h2>Fresno, CA</h2>
+      <p>April 29th, 2023</p>
+      <p class="subtext">A free music and art community festival</p>
+    </div>
+    <div class="triangle"></div>
+    <p class="count-down-title">Countdown</p>
+    <div class="counter-wrapper">
+      <div id="counter">
+        <span id="days"></span>
+        <span id="hours"></span>
+        <span id="minutes"></span>
+        <span id="seconds"></span>
       </div>
-
-      <div class=" row4 col-md-5">
-        <img class="img1" src="<?php the_field('image_1'); ?>" width="300" height="300" alt="">
-        <img class="img2" src="<?php the_field('image_2'); ?>" width="145" height="145" alt="">
-        <img class="img3" src="<?php the_field('image_3'); ?>" width="145" height="145" alt="">
+      <ul>
+        <li>Days</li>
+        <li>Hours</li>
+        <li>Minutes</li>
+        <li>Seconds</li>
+      </ul>
     </div>
-</div>
-<!-- End of first featurette -->
-<!-- Start of second featurette -->
-<div class="row featurette color">
-        <div class="col-md-77">
-            <h2 class="featurette-heading2"><?php the_field('carousel_heading'); ?></h2>
-            <p class="lead2"><?php the_field('carousel_paragraph'); ?></p>
-            <a href="#" class="button5">VIEW ALL PORCHES</a>
+  </div>
+  <!-- polygon wrapper desktop -->
+  <div class="polygon-wrapper-desktop">
+    <div class="rectangle">
+      <h2>Fresno, CA</h2>
+      <p>April 29th, 2023</p>
+      <p class="subtext">A free music and art community festival</p>
+    </div>
+    <div class="triangle">
+      <p class="count-down-title">Countdown</p>
+      <div class="counter-wrapper">
+        <div id="counter">
+          <span id="days-desk"></span>
+          <span id="hours-desk"></span>
+          <span id="minutes-desk"></span>
+          <span id="seconds-desk"></span>
         </div>
-
-        <div class="col-md-57">
-        <div id="porch_carousel" class="carousel" data-bs-ride="carousel">
-    <div class="carousel-indicators" id="porch_indicator">
-        <button type="button" data-bs-target="#porch_carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#porch_carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#porch_carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        <button type="button" data-bs-target="#porch_carousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
-        <button type="button" data-bs-target="#porch_carousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
-        <button type="button" data-bs-target="#porch_carousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
-    </div>
-    <div class="carousel-inner" id="test_carousel">
-        <!-- card 1 -->
-        <div class="carousel-item active">
-                <div class="card">
-                    <h5 class="card-title">ROCKING PORCH</h5>
-                    <p class="address">123 Olive Dr. Fresno CA 93728</p>
-                    <img class="image1" src="<?php the_field('card_image_1'); ?>" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="item">YOGA</p>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <p class="time"> <b> 11am - 2pm </b> </p>
-                        <a href="#" class="button4">SEE LINEUP</a>
-                    </div>
-                </div>
-            </div>
-            <!-- card 2 -->
-        <div class="carousel-item">
-        <div class="card">
-        <h5 class="card-title">GREEN HOUSE PORCH</h5>
-        <p class="address">123 Olive Dr. Fresno CA 93728</p>
-        <img class="image1" src="<?php the_field('card_image_2'); ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <p class="item">YOGA</p>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class="time"> <b> 11am - 2pm </b> </p>
-        <a href="#" class="button4">SEE LINEUP</a>
-    </div>
-    </div>
-        </div>
-        <!-- card 3 -->
-        <div class="carousel-item">
-        <div class="card">
-        <h5 class="card-title">YOGA BEARS</h5>
-        <p class="address">123 Olive Dr. Fresno CA 93728</p>
-        <img class="image1" src="<?php the_field('card_image_3'); ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <p class="item">YOGA</p>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class="time"> <b> 11am - 2pm </b> </p>
-        <a href="#" class="button4">SEE LINEUP</a>
-    </div>
-    </div>
-        </div>
-        <!-- card 4 -->
-        <div class="carousel-item">
-        <div class="card">
-        <h5 class="card-title">THE SMITHS</h5>
-        <p class="address">123 Olive Dr. Fresno CA 93728</p>
-        <img class="image1" src="<?php the_field('card_image_4'); ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <p class="item">YOGA</p>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class="time"> <b> 11am - 2pm </b> </p>
-        <a href="#" class="button4">SEE LINEUP</a>
-    </div>
-    </div>
-        </div>
-        <!-- card 5 -->
-        <div class="carousel-item">
-        <div class="card">
-        <h5 class="card-title">THE HOUSE ON OLIVE</h5>
-        <p class="address">123 Olive Dr. Fresno CA 93728</p>
-        <img class="image1" src="<?php the_field('card_image_5'); ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <p class="item">YOGA</p>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class="time"> <b> 11am - 2pm </b> </p>
-        <a href="#" class="button4">SEE LINEUP</a>
-    </div>
-    </div>
-        </div>
-        <!-- card 6 -->
-        <div class="carousel-item">
-        <div class="card">
-        <h5 class="card-title">SPOKEASY</h5>
-        <p class="address">123 Olive Dr. Fresno CA 93728</p>
-        <img class="image1" src="<?php the_field('card_image_6'); ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <p class="item">YOGA</p>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p class="time"> <b> 11am - 2pm </b> </p>
-        <a href="#" class="button4">SEE LINEUP</a>
-    </div>
-    </div>
-        </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#porch_carousel" data-bs-slide="prev" id="test_prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#porch_carousel" data-bs-slide="next" id="test_next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-</div>
-<!-- End of second featurette -->
-
-<!-- Start of third featurette -->
-<div class="row featurette">
-      <div class="col-md-78">
-        <h2 class="featurette-heading"><?php the_field('heading_part_1'); ?> <br> <?php the_field('heading_part_2'); ?></h2>
-        <p class="lead"><?php the_field('paragraph_part_1'); ?></p>
-        <a href="#" class="button2">VISIT MAP</a>
+        <ul>
+          <li>Days</li>
+          <li>Hours</li>
+          <li>Minutes</li>
+          <li>Seconds</li>
+        </ul>
       </div>
-
-      <div class="col-md-5">
-        <!-- MAP PLACEHOLDER -->
-        <img class="map" src="<?php the_field('placeholder'); ?>" width="500" height="" alt="">
-
+    </div>
+  </div>
+  <!-- end -->
+  <section class="section-wrapper">
+  <!-- light text section -->
+    <div class="light-section text-and-button">
+      <div class="wrapper">
+        <h2>Building Community Through Music and Art</h2>
+        <p>Porches throughout Fresno’s historic Tower neighborhood host performances in a day of revelry celebrating local music and art! We do this through the collaboration of Tower residents who provide venue for local musicians to perform.</p>
+        <button>View Porch Map</button>
       </div>
-</div>
-<!-- End of third featurette -->
-
-<!-- Card section start -->
-<div class="back">
-    <!-- Card 1 -->
-    <div class="box1">
-        <img class="ticket" src="<?php the_field('ticket_image'); ?>" width="130" height="130">
-        <h2 class="h22"><?php the_field('ticket_heading'); ?></h2>
-        <p class="p2"> <b><?php the_field('ticket_paragraph_1'); ?></b> <?php the_field('ticket_paragraph_2'); ?></p>
-        <a href="url" class="spon">BECOME A SPONSOR</a>
     </div>
-    
-    <!-- Card 2 -->
-    <div class="box2">
-        <img class="food" src="<?php the_field('food_image'); ?>" width="130" height="130">
-        <h2 class="h22"><?php the_field('food_heading'); ?></h2>
-        <p class="p2"><?php the_field('food_paragraph'); ?></p>
-        <a href="url" class="spon">PORCHES WITH FOOD</a>
-    </div>
-
-    <!-- Card 3 -->
-    <div class="box3">
-        <img class="parking" src="<?php the_field('parking_image'); ?>" width="130" height="130">
-        <h2 class="h22"><?php the_field('parking_heading'); ?></h2>
-        <p class="p2"><?php the_field('parking_paragraph'); ?></p>
-        <a href="url" class="spon3">VIEW MAP</a>
-    </div>
-
-    <!-- Card 4 -->
-    <div class="box4">
-        <img class="merch" src="<?php the_field('merch_image'); ?>" width="130" height="130">
-        <h2 class="h22"><?php the_field('merch_heading'); ?></h2>
-        <p class="p2"><?php the_field('merch_paragraph'); ?></p>
-        <a href="url" class="spon4">MERCH STORE</a>
-    </div>
-</div>
-<!-- Card section end -->
-
-<!-- Start of forth featurette -->
-<div class="row featurette">
-      <div class="col-md-71">
-        <h2 class="featurette-heading3"><?php the_field('sponsors_heading'); ?></h2>
-        <p class="lead3"><?php the_field('sponsors_paragraph'); ?></p>
-        <a href="#" class="button3">BECOME A SPONSOR</a>
-      </div>
-        <br>
-      <div class="col-md-58">
-        <div class="grid">
-            <div class="one">
-                <img src="<?php the_field('ragging'); ?>" alt="Ragging" >
-            </div>
-
-            <div class="two">
-                <img src="<?php the_field('spokeasy'); ?>" alt="Spokeasy" >
-            </div>
-
-            <div class="three">
-                <img src="<?php the_field('neighborhood_thrift'); ?>" alt="Neighborhood Thrift" >
-            </div>
-
-            <div class="four">
-                <img src="<?php the_field('hi-top_coffee'); ?>" alt="Hi-top Coffee" >
-            </div>
-            <div class=" five">
-                <img src="<?php the_field('the_brass_unicorn'); ?>" alt="The Brass Unicorn" >
-            </div>
-            
-            <div class="six">
-                <img src="<?php the_field('cf'); ?>" alt="CF" >
-            </div>
-
-            <div class="seven">
-                <img src="<?php the_field('blkmktplc'); ?>" alt="Blkmktplc" >
-            </div>
+  <!-- three up images -->
+    <div class="three-up">
+      <div class="three-up-wrapper">
+        <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-6-1.png" alt="">
+        <div class="image-row">
+          <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-7-1.png" alt="">
+          <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-8.png" alt="">
         </div>
-        
-
       </div>
-</div>
-<!-- End of forth featurette -->
-
-
+    </div>
+  </section>
+  <!-- dark text section -->
+  <section class="dark-section text-and-button">
+    <div class="wrapper">
+      <h2>Call for Porches</h2>
+      <p>Do you have a porch in the Tower that would make a sweet stage? Tower Porchfest 2023 will happen on Saturday, April 29 from noon to 8 PM in Fresno’s Tower District neighborhood! Porch Host signups are open now through March 6, 2023.</p>
+      <button>View All Porches</button>
+    </div>
+    <div class="wrapper">
+      <h2>Call for Porches</h2>
+      <p>Do you have a porch in the Tower that would make a sweet stage? Tower Porchfest 2023 will happen on Saturday, April 29 from noon to 8 PM in Fresno’s Tower District neighborhood! Porch Host signups are open now through March 6, 2023.</p>
+      <button>View All Porches</button>
+    </div>
+  </section>
+  <!-- Future Carousel -->
+  <!-- light text section -->
+  <section class="section-wrapper">
+    <div class="light-section text-and-button">
+      <div class="wrapper">
+        <h2>Tower Porchfest Map</h2>
+        <p>Tower Porchfest takes place in the Historic Tower District neighborhood in Fresno, California. Made up of an incredible array of classic housing types – ranging from granny flats, townhouses, and apartments to craftsman bungalows and mansions.</p>
+        <button>Visit Map</button>
+      </div>
+    </div>
+    <div class="map-wrapper">
+      <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-6.jpg" alt="">
+    </div>
+  </section>
+  <!-- icons + cards section -->
+  <section class="container icons-cards-container">
+    <!-- <div class="row"> -->
+      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+    <!-- </div> -->
+  </section>
+<!-- Sponsors Section -->
+  <section>
+    <div class="light-section text-and-button">
+      <div class="wrapper">
+        <h2>Thank You Sponsors</h2>
+        <p> We thank our sponsors who help us be the best we can be! Last year we had 60 porches with over 130 performances. There were over 5,000 views of the interactive map and our Social Media grew to 2,000+ followers (and counting). On behalf of the Tower Porchfest Committee, we ask you to consider sponsoring this awesome event!</p>
+        <button>Become a Sponsor</button>
+      </div>
+    </div>
+    <div class="sponsors">
+      <section class="logo-container">
+        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/raging-records-logo-1.png">
+        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/spokeasy-logo-1.png">
+        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/neighborhood-thrift-1.png">
+        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/the-brass-unicorn-1.png">
+        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/blkmktplc-1.png">
+        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/hi-top-coffee-1.png">
+      </section>
+</main>
+<!-- Footer must be included for navigation to display -->
 <?php get_footer(); ?>
