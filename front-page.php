1fccc1e7a1adb37f39e59e8d0dfd43bc4e89b304
Crystal Maldonado <cmaldonado@bitwiseindustries.com>
2023-04-09 17:01:37 -0700

    Added text and three up acf

--- front-page.php
+++ front-page.php
@@ -8,27 +8,8 @@
 ?>
 
 <?php get_header(); ?>
-
-<?php if( have_rows('front_page_group') ): ?>
-    <?php while( have_rows('front_page_group') ): the_row(); ?>
-    <?php if( have_rows('where_when_and_countdown_timer') ): ?>
-      <?php while( have_rows('where_when_and_countdown_timer') ): the_row(); 
-
-        // Get sub field values.
-        $city_state = get_sub_field('city_state');
-        $festival_date = get_sub_field('festival_date');
-        $subtext = get_sub_field('subtext');
-
-        // Passes the countdown to date in a variable and gives to countdown.js
-        wp_localize_script( 'countdown', 'countdownData', array(
-          'festivalDate' => $festival_date
-        ) );
-
-
-        ?>
-
-<?php get_header(); ?>
 <main class="front-page-styles">
+  
   <!-- grabs featured image -->
   <?php
   if ( has_post_thumbnail() ) {
@@ -37,135 +18,168 @@
     echo '</div>';
   }
   ?>
-  
-  <!-- date and counter -->
-  <div class="polygon-wrapper">
-    <div class="rectangle">
-      <h2><?php echo $city_state;?></h2>
-      <p><?php echo $festival_date;?></p>
-      <p class="subtext"><?php echo $subtext;?></p>
-    </div>
-    <div class="triangle"></div>
-    <p class="count-down-title">Countdown</p>
-    <div class="counter-wrapper">
-      <div id="counter">
-        <span id="days"></span>
-        <span id="hours"></span>
-        <span id="minutes"></span>
-        <span id="seconds"></span>
-      </div>
-      <ul>
-        <li>Days</li>
-        <li>Hours</li>
-        <li>Minutes</li>
-        <li>Seconds</li>
-      </ul>
-    </div>
-  </div>
-  <!-- polygon wrapper desktop -->
-  <div class="polygon-wrapper-desktop">
-    <div class="rectangle">
-      <h2>Fresno, CA</h2>
-      <p>April 29th, 2023</p>
-      <p class="subtext">A free music and art community festival</p>
-    </div>
-    <div class="triangle">
-      <p class="count-down-title">Countdown</p>
-      <div class="counter-wrapper">
-        <div id="counter">
-          <span id="days-desk"></span>
-          <span id="hours-desk"></span>
-          <span id="minutes-desk"></span>
-          <span id="seconds-desk"></span>
-        </div>
-        <ul>
-          <li>Days</li>
-          <li>Hours</li>
-          <li>Minutes</li>
-          <li>Seconds</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <!-- end -->
-  <section class="section-wrapper">
-  <!-- light text section -->
-    <div class="light-section text-and-button">
-      <div class="wrapper">
-        <h2>Building Community Through Music and Art</h2>
-        <p>Porches throughout Fresno’s historic Tower neighborhood host performances in a day of revelry celebrating local music and art! We do this through the collaboration of Tower residents who provide venue for local musicians to perform.</p>
-        <button>View Porch Map</button>
-      </div>
-    </div>
-  <!-- three up images -->
-    <div class="three-up">
-      <div class="three-up-wrapper">
-        <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-6-1.png" alt="">
-        <div class="image-row">
-          <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-7-1.png" alt="">
-          <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-8.png" alt="">
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- dark text section -->
-  <section class="dark-section text-and-button">
-    <div class="wrapper">
-      <h2>Call for Porches</h2>
-      <p>Do you have a porch in the Tower that would make a sweet stage? Tower Porchfest 2023 will happen on Saturday, April 29 from noon to 8 PM in Fresno’s Tower District neighborhood! Porch Host signups are open now through March 6, 2023.</p>
-      <button>View All Porches</button>
-    </div>
-    <div class="wrapper">
-      <h2>Call for Porches</h2>
-      <p>Do you have a porch in the Tower that would make a sweet stage? Tower Porchfest 2023 will happen on Saturday, April 29 from noon to 8 PM in Fresno’s Tower District neighborhood! Porch Host signups are open now through March 6, 2023.</p>
-      <button>View All Porches</button>
-    </div>
-  </section>
-  <!-- Future Carousel -->
-  <!-- light text section -->
-  <section class="section-wrapper">
-    <div class="light-section text-and-button">
-      <div class="wrapper">
-        <h2>Tower Porchfest Map</h2>
-        <p>Tower Porchfest takes place in the Historic Tower District neighborhood in Fresno, California. Made up of an incredible array of classic housing types – ranging from granny flats, townhouses, and apartments to craftsman bungalows and mansions.</p>
-        <button>Visit Map</button>
-      </div>
-    </div>
-    <div class="map-wrapper">
-      <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-6.jpg" alt="">
-    </div>
-  </section>
-  <!-- icons + cards section -->
-  <section class="container icons-cards-container">
-    <!-- <div class="row"> -->
-      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
-      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
-      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
-      <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
-    <!-- </div> -->
-  </section>
-<!-- Sponsors Section -->
-  <section>
-    <div class="light-section text-and-button">
-      <div class="wrapper">
-        <h2>Thank You Sponsors</h2>
-        <p> We thank our sponsors who help us be the best we can be! Last year we had 60 porches with over 130 performances. There were over 5,000 views of the interactive map and our Social Media grew to 2,000+ followers (and counting). On behalf of the Tower Porchfest Committee, we ask you to consider sponsoring this awesome event!</p>
-        <button>Become a Sponsor</button>
-      </div>
-    </div>
-    <div class="sponsors">
-      <section class="logo-container">
-        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/raging-records-logo-1.png">
-        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/spokeasy-logo-1.png">
-        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/neighborhood-thrift-1.png">
-        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/the-brass-unicorn-1.png">
-        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/blkmktplc-1.png">
-        <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/hi-top-coffee-1.png">
-      </section>
-</main>
+  <!-- connects to ACF group Front Page -->
+   <?php if( have_rows('front_page_group') ): ?>
+    <?php while( have_rows('front_page_group') ): the_row(); ?>
+    <!-- date and counter -->
 
-      <?php endwhile; ?>
-<?php endif; ?>
+      <?php if( have_rows('where_when_and_countdown_timer') ): ?>
+        <?php while( have_rows('where_when_and_countdown_timer') ): the_row(); 
+
+          // Get sub field values.
+          $city_state = get_sub_field('city_state');
+          $festival_date = get_sub_field('festival_date');
+          $subtext = get_sub_field('subtext');
+          $countdown_title = get_sub_field('countdown_title');
+
+          // Passes the countdown to date in a variable and gives to countdown.js
+          wp_localize_script( 'countdown', 'countdownData', array(
+            'festivalDate' => $festival_date
+          ) );
+        ?>
+          <div class="polygon-wrapper">
+            <div class="rectangle">
+              <h2><?php echo $city_state;?></h2>
+              <p><?php echo $festival_date;?></p>
+              <p class="subtext"><?php echo $subtext;?></p>
+            </div>
+            <div class="triangle"></div>
+            <p class="count-down-title"><?php echo $countdown_title;?></p>
+            <div class="counter-wrapper">
+              <div id="counter">
+                <span id="days"></span>
+                <span id="hours"></span>
+                <span id="minutes"></span>
+                <span id="seconds"></span>
+              </div>
+              <ul>
+                <li>Days</li>
+                <li>Hours</li>
+                <li>Minutes</li>
+                <li>Seconds</li>
+              </ul>
+            </div>
+          </div>
+          <!-- polygon wrapper desktop -->
+          <div class="polygon-wrapper-desktop">
+            <div class="rectangle">
+              <h2><?php echo $city_state;?></h2>
+              <p><?php echo $festival_date;?></p>
+              <p class="subtext"><?php echo $subtext;?></p>
+            </div>
+            <div class="triangle">
+              <p class="count-down-title"><?php echo $countdown_title;?></p>
+              <div class="counter-wrapper">
+                <div id="counter">
+                  <span id="days-desk"></span>
+                  <span id="hours-desk"></span>
+                  <span id="minutes-desk"></span>
+                  <span id="seconds-desk"></span>
+                </div>
+                <ul>
+                  <li>Days</li>
+                  <li>Hours</li>
+                  <li>Minutes</li>
+                  <li>Seconds</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+  <!-- end -->
+  <!-- Text and Three Up Images -->
+      <?php if( have_rows('text_and_three_up_images') ): ?>
+        <?php while( have_rows('text_and_three_up_images') ): the_row(); 
+          // Get sub field values.
+          $title = get_sub_field('title');
+          $paragraph = get_sub_field('paragraph');
+          $button_text = get_sub_field('button_text');
+          $button_link = get_sub_field('button_link');
+          $big_image = get_sub_field('big_image');
+          $small_left_image = get_sub_field('small_left_image');
+          $small_right_image = get_sub_field('small_right_image');
+        ?>
+          <section class="section-wrapper">
+          <!-- light text section -->
+            <div class="light-section text-and-button">
+              <div class="wrapper">
+                <h2><?php echo $title;?></h2>
+                <p><?php echo $paragraph;?></p>
+                <button><?php echo $button_text;?></button>
+              </div>
+            </div>
+          <!-- three up images -->
+            <div class="three-up">
+              <div class="three-up-wrapper">
+                <img src="<?php echo $big_image;?>" alt="">
+                <div class="image-row">
+                  <img src="<?php echo $small_left_image;?>" alt="">
+                  <img src="<?php echo $small_right_image;?>" alt="">
+                </div>
+              </div>
+            </div>
+          </section>
+        <?php endwhile; ?>
+      <?php endif; ?>
+  <!-- end -->    
+        <!-- dark text section -->
+        <section class="dark-section text-and-button">
+          <div class="wrapper">
+            <h2>Call for Porches</h2>
+            <p>Do you have a porch in the Tower that would make a sweet stage? Tower Porchfest 2023 will happen on Saturday, April 29 from noon to 8 PM in Fresno’s Tower District neighborhood! Porch Host signups are open now through March 6, 2023.</p>
+            <button>View All Porches</button>
+          </div>
+          <div class="wrapper">
+            <h2>Call for Porches</h2>
+            <p>Do you have a porch in the Tower that would make a sweet stage? Tower Porchfest 2023 will happen on Saturday, April 29 from noon to 8 PM in Fresno’s Tower District neighborhood! Porch Host signups are open now through March 6, 2023.</p>
+            <button>View All Porches</button>
+          </div>
+        </section>
+        <!-- Future Carousel -->
+        <!-- light text section -->
+        <section class="section-wrapper">
+          <div class="light-section text-and-button">
+            <div class="wrapper">
+              <h2>Tower Porchfest Map</h2>
+              <p>Tower Porchfest takes place in the Historic Tower District neighborhood in Fresno, California. Made up of an incredible array of classic housing types – ranging from granny flats, townhouses, and apartments to craftsman bungalows and mansions.</p>
+              <button>Visit Map</button>
+            </div>
+          </div>
+          <div class="map-wrapper">
+            <img src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/Rectangle-6.jpg" alt="">
+          </div>
+        </section>
+        <!-- icons + cards section -->
+        <section class="container icons-cards-container">
+          <!-- <div class="row"> -->
+            <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+            <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+            <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+            <? get_template_part( 'template-parts/content', 'icons-cards' ); ?>
+          <!-- </div> -->
+        </section>
+      <!-- Sponsors Section -->
+        <section>
+          <div class="light-section text-and-button">
+            <div class="wrapper">
+              <h2>Thank You Sponsors</h2>
+              <p> We thank our sponsors who help us be the best we can be! Last year we had 60 porches with over 130 performances. There were over 5,000 views of the interactive map and our Social Media grew to 2,000+ followers (and counting). On behalf of the Tower Porchfest Committee, we ask you to consider sponsoring this awesome event!</p>
+              <button>Become a Sponsor</button>
+            </div>
+          </div>
+          <div class="sponsors">
+            <section class="logo-container">
+              <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/raging-records-logo-1.png">
+              <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/spokeasy-logo-1.png">
+              <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/neighborhood-thrift-1.png">
+              <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/the-brass-unicorn-1.png">
+              <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/blkmktplc-1.png">
+              <img class="logo-style" src="http://tpf-4-6-v2.local/wp-content/uploads/2023/04/hi-top-coffee-1.png">
+            </section>
+      </main>
+
+
 <?php endwhile; ?>
 <?php endif; ?>
 
