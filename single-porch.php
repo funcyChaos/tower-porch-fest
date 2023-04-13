bfc544e7a7d331a42eb91e9e82831802bdb8754b
Austin Reilly <areilly@bitwiseindustries.com>
2023-04-13 09:26:51 -0700

    refactor(porches): change styles

--- single-porch.php
+++ single-porch.php
@@ -15,64 +15,83 @@ while(have_posts()){
 		<section class="prochesSingle">
 			<div class="singleporchImgContainer">
 			<?php 
-				if(has_post_thumbnail())the_post_thumbnail('full');
+				$imgURL = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_the_post_thumbnail_url(5);
 			?>
+				<img class="porchimage" src="<?=$imgURL?>" alt="Picture of Porch"/>
 			</div>
 			<div class="singleporchContentContainer">
 				<h2 class="porchheading"><?=the_title()?></h2>
 				<p class="porchaddress"><?=the_field('porch_address')?></p>
-				<p class="porchDescription"> <?php the_field('description'); ?> </p>
+				<?php $content = wp_strip_all_tags(get_the_content());?>
+				<p class="porchDescription"><?=$content?></p>
 				<a href="#" class="singleButton">SEE LINEUP</a>
 			</div>
 		</section>
 		<section class="categoriesBar">
-				<a href=""><?php the_field('genre'); ?></a>
-				<a href=""><?php the_field('tag_one'); ?></a>
-				<a href=""><?php the_field('tag_two'); ?></a>
+			<a href=""><?php the_field('tag_one'); ?></a>
+			<a href=""><?php the_field('tag_two'); ?></a>
+			<a href=""><?php the_field('tag_three'); ?></a>
 		</section>
 		<section class="singlepage">
 		<section class="singleArchivesSection" style="background:linear-gradient(to right, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('<?php the_field('category_background_image'); ?>'); background-position: center; background-size: cover; background-repeat: no-repeat; ">
 		</section>
 		<div class="cardContainer" >
-				<div class="SingleCatCard">
-					<div class="TagImg">
-						<img class="porchimage" src="<?php the_field('tag_image'); ?>" alt="" />
-						<div class="tag">
-							<p><?php the_field('genre'); ?></p>
+			<?php
+				$tagOne = get_field('tag_one');
+				if($tagOne){
+					?>
+						<div class="SingleCatCard">
+							<div class="TagImg">
+								<img class="porchimage" src="<?php the_field('tag_image'); ?>" alt="" />
+								<div class="tag">
+									<p><?=$tagOne?></p>
+								</div>
+							</div>
+							<div class="TagContent">
+								<a href="#" class="singleButton"><?php the_field('tag_time'); ?></a>
+								<h2 class="tagHeading" > <?php the_field('tag_heading');?> </h2>
+								<p class="porchDescription"> <?php the_field('tag_description'); ?> </p>
+							</div>
 						</div>
-					</div>
-					<div class="TagContent">
-						<a href="#" class="singleButton"><?php the_field('tag_time'); ?></a>
-						<h2 class="tagHeading" > <?php the_field('tag_heading'); ?> </h2>
-						<p class="porchDescription"> <?php the_field('tag_description'); ?> </p>
-					</div>
-				</div>
-				<div class="SingleCatCard">
-				<div class="TagImg">
-						<img class="porchimage" src="<?php the_field('tag_two_image'); ?>" alt="" />
-						<div class="tag">
-							<p><?php the_field('tag_one'); ?></p>
+					<?php
+				}
+				$tagTwo = get_field('tag_two');
+				if($tagTwo){
+					?>
+						<div class="SingleCatCard">
+						<div class="TagImg">
+								<img class="porchimage" src="<?php the_field('tag_two_image'); ?>" alt="" />
+								<div class="tag">
+									<p><?=$tagTwo?></p>
+								</div>
+							</div>
+							<div class="TagContent">
+								<a href="#" class="singleButton"><?php the_field('tag_two_time'); ?></a>
+								<h2 class="tagHeading" > <?php the_field('tag_two_heading'); ?> </h2>
+								<p class="porchDescription"> <?php the_field('tag_two_description'); ?> </p>
+							</div>
 						</div>
-					</div>
-					<div class="TagContent">
-						<a href="#" class="singleButton"><?php the_field('tag_two_time'); ?></a>
-						<h2 class="tagHeading" > <?php the_field('tag_two_heading'); ?> </h2>
-						<p class="porchDescription"> <?php the_field('tag_two_description'); ?> </p>
-					</div>
-				</div>
-				<div class="SingleCatCard">
-				<div class="TagImg">
-						<img class="porchimage" src="<?php the_field('tag_three_image'); ?>" alt="" />
-						<div class="tag">
-							<p><?php the_field('tag_two'); ?></p>
+					<?php
+				}
+				$tagThree = get_field('tag_three');
+				if($tagThree){
+					?>
+						<div class="SingleCatCard">
+						<div class="TagImg">
+								<img class="porchimage" src="<?php the_field('tag_three_image'); ?>" alt="" />
+								<div class="tag">
+									<p><?=$tagThree?></p>
+								</div>
+							</div>
+							<div class="TagContent">
+								<a href="#" class="singleButton"><?php the_field('tag_three_time'); ?></a>
+								<h2 class="tagHeading" > <?php the_field('tag_three_heading'); ?> </h2>
+								<p class="porchDescription"> <?php the_field('tag_three_description'); ?> </p>
+							</div>
 						</div>
-					</div>
-					<div class="TagContent">
-						<a href="#" class="singleButton"><?php the_field('tag_three_time'); ?></a>
-						<h2 class="tagHeading" > <?php the_field('tag_three_heading'); ?> </h2>
-						<p class="porchDescription"> <?php the_field('tag_three_description'); ?> </p>
-					</div>
-				</div>
+					<?php
+				}
+				?>
 			</div>
 	</section>
 	<?php
