8af4a05d69e263a2f3681b778524e75f08a9b548
Austin Reilly <austinreilly51@gmail.com>
2024-03-04 21:32:24 -0800

    feat(import): import works on update

--- coord-import/import.php
+++ coord-import/import.php
@@ -17,13 +17,20 @@ add_action('rest_api_init', function(){
 	]);
 });
 
-add_action('save_post_porch', function($post_id, $post, $updating){
-	$key = map_api_key;
-	$addy = get_field('porch_address', $post_id);
-	$url = urlencode("https://maps.googleapis.com/maps/api/geocode/json?key={$key}");
-	$geocode = json_decode(file_get_contents($url));
-	// $geocode = json_decode(file_get_contents(
-	// 	"https://maps.googleapis.com",
-	// ));
-	update_option('test', $geocode);
-}, 10, 3);
+add_action('save_post', function($post_id){
+	if(get_post_type($post_id) == 'porch'){
+		$key = map_api_key;
+		$address = urlencode(get_field('porch_address', $post_id));
+		$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key={$key}";
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$data = json_decode($response, true);
+		$latitude = $data['results'][0]['geometry']['location']['lat'];
+		$longitude = $data['results'][0]['geometry']['location']['lng'];
+		update_field('latitude', $latitude, $post_id);
+		update_field('longitude', $longitude, $post_id);
+	}
+}, 10, 1);
