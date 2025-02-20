55105e8a514078ac019e32707a1369d5c5f1718c
Austin Reilly <funcychaos@gmail.com>
2025-02-20 13:01:14 -0800

    fix(excess): forgot the example

--- js/performances.js
+++ js/performances.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-	const rootElement = document.getElementById("react-root");
-	if (rootElement) {
-			const root = ReactDOM.createRoot(rootElement);
-			root.render(React.createElement("h1", null, "Hello, World from React!"));
-	}
-});
-
 document.addEventListener("DOMContentLoaded", ()=>{
 	const rootElement = document.getElementById("react_root")
 	if(rootElement){
