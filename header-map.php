e73e0e052303087baf7b2af1584817bbd09e03e7
null1space <200291550+null1space@users.noreply.github.com>
2025-04-17 12:42:52 -0700

    revert(awesomefont): revert change to awesomefont

--- header-map.php
+++ header-map.php
@@ -21,8 +21,8 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans:wght@500&display=swap" rel="stylesheet">
-	<!-- Add Font Awesome 6 CSS from CDN -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<!-- font awesome -->
+	<script src="https://kit.fontawesome.com/3c576d03cf.js" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 	<style>
 		#rmp_menu_trigger-1585{
